c8a0e87ba01c4768bcc4d402ecaa71fa1f5ee305
ImBlakeHotz <79678872+ImBlakeHotz@users.noreply.github.com>
2023-01-31 10:17:30 -0500

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1649588.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 119679867, 119679875, 119679877, 119679891, 119679894, 119679901 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/119679860/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/119679861/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/119679859/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/119679864/1/basis.wasm.js', 'wasmUrl' : 'files/assets/119679865/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/119679863/1/basis.js', 'preload' : false},
+];
